6ab74173b2135c6343f0107a45e5a9035a62a0c0
Jeff Goldberg <goldbergjeffrey@yahoo.com>
2016-05-15 19:11:34 -0400

    getAppDataSegments Module Added
    getAppDataSegments seeks to inform the processor that it can continue
    with applying metrics after the metrics library qlik app has been
    updated and changed on the server.

--- GovernedMetricsService/lib/reloadmetrics.js
+++ GovernedMetricsService/lib/reloadmetrics.js
@@ -2,6 +2,7 @@ var Promise = require('bluebird');
 var config = require('../config/config');
 var winston = require('winston');
 var qrsInteract = require('./qrsinteractions');
+var appDataSegment = require('./getAppDataSegment');
 
 //set up logging
 var logger = new (winston.Logger)({
@@ -14,60 +15,82 @@ var logger = new (winston.Logger)({
 
 var reloadMetrics = {
 
-	reloadMetrics: function(taskName)
+	reloadMetrics: function(app, taskName)
 	{
+		var currContentHash;
 		return new Promise(function(resolve, reject)
 		{
 			
-			logger.info('reloadMetrics::Starting Task::' + taskName, {module: 'reloadmetrics'});
-			var path = "https://" + config.hostname + ":" + config.qrsPort + "/qrs/task/start/synchronous";
-			path += "?xrfkey=ABCDEFG123456789&name=" + taskName;
-			logger.debug('reloadMetrics::PATH::' + path, {module: 'reloadmetrics'});
-			qrsInteract.post(path)
+			appDataSegment.getAppDataSegment(app)
 			.then(function(result)
 			{
-				var taskId = result.value;
-				if(typeof result =='object')
+				currentContentHash = result[0].contentHash;
+				logger.info('reloadMetrics::Starting Task::' + taskName, {module: 'reloadmetrics'});
+				var path = "https://" + config.hostname + ":" + config.qrsPort + "/qrs/task/start/synchronous";
+				path += "?xrfkey=ABCDEFG123456789&name=" + taskName;
+				logger.debug('reloadMetrics::PATH::' + path, {module: 'reloadmetrics'});
+				qrsInteract.post(path)
+				.then(function(result)
 				{
-					logger.info('reloadMetrics::Task Started::' + taskName, {module: 'reloadmetrics'});
-					progressCheck(taskId, function(error, result)
+					var taskId = result.value;
+					if(typeof result =='object')
 					{
-						//now that we are done, let's evaluate the result
-						if(error)
+						logger.info('reloadMetrics::Task Started::' + taskName, {module: 'reloadmetrics'});
+						progressCheck(taskId, function(error, result)
 						{
-							logger.error('reloadMetrics::progressCheck::' + error, {module: 'reloadmetrics'});
-							reject(new Error(error));
-						}
-						else
-						{
-							logger.info('reloadMetrics::Task Complete::' + taskName, {module: 'reloadmetrics'});
-							var path2 = "https://" + config.hostname + ":" + config.qrsPort + "/qrs/executionresult";
-							path2 += "?xrfkey=ABCDEFG123456789&filter=executionid eq " + taskId;
-							logger.debug('reloadMetrics::PATH::' + path2, {module: 'reloadmetrics'});
-							qrsInteract.get(path2)
-							.then(function(reloadInfo)
+							//now that we are done, let's evaluate the result
+							if(error)
 							{
-
-								logger.info('reloadMetrics::Task Completed in ' + reloadInfo[0].duration + 'milliseconds', {module: 'reloadmetrics'});
-								resolve('Task Completed in ' + reloadInfo[0].duration + ' milliseconds');
-							})
-							.catch(function(error)
-							{
-								logger.error('reloadMetrics::Task Failure::' + error, {module: 'reloadmetrics'});
+								logger.error('reloadMetrics::progressCheck::' + error, {module: 'reloadmetrics'});
 								reject(new Error(error));
-							});
-						}
-					});
-				}
-				else
+							}
+							else
+							{
+								logger.info('reloadMetrics::Task Complete::' + taskName, {module: 'reloadmetrics'});
+								var path2 = "https://" + config.hostname + ":" + config.qrsPort + "/qrs/executionresult";
+								path2 += "?xrfkey=ABCDEFG123456789&filter=executionid eq " + taskId;
+								logger.debug('reloadMetrics::PATH::' + path2, {module: 'reloadmetrics'});
+								qrsInteract.get(path2)
+								.then(function(reloadInfo)
+								{
+
+									logger.info('reloadMetrics::Task Completed in ' + reloadInfo[0].duration + 'milliseconds', {module: 'reloadmetrics'});
+									hashCheck(app,currentContentHash,function(error,result)
+									{
+										if(error)
+										{
+											logger.error('reloadMetrics::progressCheck::' + error, {module: 'reloadMetrics'});
+											reject(new Error(error));
+										}
+										else
+										{
+											logger.info('reloadMetrics::Content Hash changed on app data segment for ' + app.name, {module: 'reloadmetrics'});
+											resolve('Task Completed in ' + reloadInfo[0].duration + ' milliseconds');
+										}
+									})									
+								})
+								.catch(function(error)
+								{
+									logger.error('reloadMetrics::Task Failure::' + error, {module: 'reloadmetrics'});
+									reject(new Error(error));
+								});
+							}
+						});
+					}
+					else
+					{
+						logger.error('reloadMetrics::' + result, {module: 'reloadmetrics'});
+						reject(new Error(result));
+					}
+				})
+				.catch(function(error)
 				{
-					logger.error('reloadMetrics::' + result, {module: 'reloadmetrics'});
-					reject(new Error(result));
-				}
+					reject(new Error(error));
+				});
 			})
 			.catch(function(error)
 			{
-				reject(new Error(error));
+				reject(error);
 			});
 		});
 	}
@@ -101,5 +124,30 @@ function progressCheck(id, callback)
 	});
 }
 
+function hashCheck(app, currentHash, callback)
+{
+	var newHash;
+	appDataSegment.getAppDataSegment(app)
+	.then(function(result)
+	{
+		logger.debug('reloadMetrics::hashCheck::New Hash::' + result[0].contentHash, {module: 'reloadMetrics'});
+		newHash = result[0].contentHash;
+		if(newHash===currentHash)
+		{
+			logger.debug('reloadMetrics::hashCheck::Has has not changed::' + newHash + '==' + currentHash, {module: 'reloadMetrics'});
+			hashCheck(app,currentHash, callback);
+		}
+		else
+		{
+			logger.debug('reloadMetrics::hashCheck::Has has changed::' + newHash + '!=' + currentHash, {module: 'reloadMetrics'});
+			callback(null, 'Hash Changed');
+		}
+	})
+	.catch(function(error)
+	{
+		logger.error('reloadMetrics::hashCheck::' + JSON.stringify(error), {module: 'reloadMetrics'});
+		callback(error);
+	});
+}
 
 module.exports = reloadMetrics;
