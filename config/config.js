344aac567d9b62846cd5d3c81884441ab42dec2e
Jeffrey Goldberg <JOG@qlik.com>
2017-01-17 15:15:37 -0500

    updates to dauledk code

--- config/config.js
+++ config/config.js
@@ -3,18 +3,16 @@ var fs = require('fs');
 var extend = require('extend');
 var installConfig;
 
-var configPath = path.join(__dirname,'/../config/');
+var configPath = path.join(__dirname, '/../config/');
 var dir = fs.readdirSync(configPath);
-dir.forEach(function(file)
-{
-    if(file==='installConfig.js')
-    {
+dir.forEach(function(file) {
+    if (file === 'installConfig.js') {
         installConfig = require('./installConfig');
     }
 })
 
 var certPath = path.join(process.env.programdata, '/Qlik/Sense/Repository/Exported Certificates/.Local Certificates');
-var logPath = path.join(__dirname,'/../log/');
+var logPath = path.join(__dirname, '/../log/');
 var logFile = logPath + 'gms.log';
 
 var globalHostname = "localhost";
@@ -22,63 +20,57 @@ var friendlyHostname;
 var qrsHostname;
 var certPathBackup;
 
-if(certPathBackup !== undefined)
-{
-	certPath = certPathBackup;
+if (certPathBackup !== undefined) {
+    certPath = certPathBackup;
 }
 
 var config = {
-	certificates: {
-		certPath: certPath,
-		client: path.resolve(certPath, 'client.pem'),
-		client_key: path.resolve(certPath,'client_key.pem'),
-		server: path.resolve(certPath, 'server.pem'),
-		server_key: path.resolve(certPath, 'server_key.pem'),
-		root: path.resolve(certPath,'root.pem')
-	},
-	gms: {
-		version: '1.5.3.0',
-		port: 8590,
-		hostname: friendlyHostname !== undefined ? friendlyHostname : globalHostname,
+    certificates: {
+        certPath: certPath,
+        client: path.resolve(certPath, 'client.pem'),
+        client_key: path.resolve(certPath, 'client_key.pem'),
+        server: path.resolve(certPath, 'server.pem'),
+        server_key: path.resolve(certPath, 'server_key.pem'),
+        root: path.resolve(certPath, 'root.pem')
+    },
+    gms: {
+        version: '1.5.3.0',
+        port: 8590,
+        hostname: friendlyHostname !== undefined ? friendlyHostname : globalHostname,
         routePath: path.join(__dirname, '/../routes/'),
-		publicPath: path.join(__dirname, '/../public/'),
-		appPath: path.join(__dirname, '/../app/'),
-		appName: 'Metrics Library',
-		customPropName: 'ManagedMasterItems',
-    metricLibraryInputPropName: 'MetricsLibraryInput',
-		taskName: 'Reload Metrics Library',
-		objectManagementTimeout: 60000
-	},
-	engine: {
-		enginePort: 4747,
-		hostname: qrsHostname !== undefined ? qrsHostname : globalHostname,
-		repoAccount: 'UserDirectory=Internal;UserId=sa_repository'
-	},
-	logging: {
-		logPath: logPath,
-		logFile: logFile,
-		logLevel: 'debug'
-	},
-	qrs: {
-		localCertPath: certPath,
-		hostname: qrsHostname !== undefined ? qrsHostname : globalHostname,
-		repoAccountUserDirectory: 'INTERNAL',
-		repoAccountUserId: 'sa_repository',
-		changeInterval: 15
-	}
+        publicPath: path.join(__dirname, '/../public/'),
+        appPath: path.join(__dirname, '/../app/'),
+        appName: 'Metrics Library',
+        customPropName: 'ManagedMasterItems',
+        masterLibrarySourcePropName: 'MasterLibrarySource',
+        taskName: 'Reload Metrics Library',
+        objectManagementTimeout: 60000
+    },
+    engine: {
+        enginePort: 4747,
+        hostname: qrsHostname !== undefined ? qrsHostname : globalHostname,
+        repoAccount: 'UserDirectory=Internal;UserId=sa_repository'
+    },
+    logging: {
+        logPath: logPath,
+        logFile: logFile,
+        logLevel: 'debug'
+    },
+    qrs: {
+        localCertPath: certPath,
+        hostname: qrsHostname !== undefined ? qrsHostname : globalHostname,
+        repoAccountUserDirectory: 'INTERNAL',
+        repoAccountUserId: 'sa_repository',
+        changeInterval: 15
+    }
 };
 
-if(friendlyHostname !==undefined || qrsHostname !== undefined || certPathBackup !== undefined)
-{
-	var mergedConfig = config;
-}
-else if(installConfig !== undefined)
-{
-	var mergedConfig = extend(true, config, installConfig);
-}
-else
-{
-	var mergedConfig = config;
+if (friendlyHostname !== undefined || qrsHostname !== undefined || certPathBackup !== undefined) {
+    var mergedConfig = config;
+} else if (installConfig !== undefined) {
+    var mergedConfig = extend(true, config, installConfig);
+} else {
+    var mergedConfig = config;
 }
 
 module.exports = mergedConfig;
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({extended: false});
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
 var hypercube = require('../lib/setCubeDims');
 var worker = require('../lib/dowork');
 var getdoc = require('../lib/getdocid');
@@ -11,218 +11,185 @@ var config = require('../config/config');
 require('winston-daily-rotate-file');
 
 //set up logging
-var logger = new (winston.Logger)({
-	level: config.logging.logLevel,
-	transports: [
-      new (winston.transports.Console)(),
-      new (winston.transports.DailyRotateFile)({ filename: config.logging.logFile, prepend: true})
+var logger = new(winston.Logger)({
+    level: config.logging.logLevel,
+    transports: [
+        new(winston.transports.Console)(),
+        new(winston.transports.DailyRotateFile)({ filename: config.logging.logFile, prepend: true })
     ]
 });
 
-router.use(function(req,res,next){
-	res.header("Access-Control-Allow-Origin","*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 router.route('/')
-	.get(function(request,response){
-		logger.info('default route called', {module: 'routes'});
-		var cube = hypercube.setCubeDefault();
-		worker.doWork(cube,function(error,result){
-			if(error)
-			{
-				logger.error('default route failure::' + error , {module: 'routes'});
-				response.status(400).json("Bad Request");
-			}
-			else
-			{
-				//for my reference so I can see how to send responses.
-				//response.status(200).json(result.connection.ws.url);
-				logger.info('default route success', {module: 'routes'});
-				response.status(200).json(result);
-			}
-		});
-	})
-	.post(parseUrlencoded, function(request,response){
-		var result = worker.doWork(hypercube.setCubeDims(request.body));
-		response.status(200).json(result);
-	});
+    .get(function(request, response) {
+        logger.info('default route called', { module: 'routes' });
+        var cube = hypercube.setCubeDefault();
+        worker.doWork(cube, function(error, result) {
+            if (error) {
+                logger.error('default route failure::' + error, { module: 'routes' });
+                response.status(400).json("Bad Request");
+            } else {
+                //for my reference so I can see how to send responses.
+                //response.status(200).json(result.connection.ws.url);
+                logger.info('default route success', { module: 'routes' });
+                response.status(200).json(result);
+            }
+        });
+    })
+    .post(parseUrlencoded, function(request, response) {
+        var result = worker.doWork(hypercube.setCubeDims(request.body));
+        response.status(200).json(result);
+    });
 
 router.route('/testpage')
-	.get(function(request,response)
-	{
-		var options = {
-			root: config.gms.appPath
-		};
-		response.sendFile('index.htm', options, function(err)
-		{
-			if(err)
-			{
-				console.log(err);
-				response.status(err.status).end();
-			}
-		});
-	});
+    .get(function(request, response) {
+        var options = {
+            root: config.gms.appPath
+        };
+        response.sendFile('index.htm', options, function(err) {
+            if (err) {
+                console.log(err);
+                response.status(err.status).end();
+            }
+        });
+    });
 
 //for testing getDocId method
 router.route('/getdocid')
-	.get(function(request,response)
-	{
-		logger.info('GET getdocid for '+  config.gms.appName, {module: 'routes'});
-		worker.getDoc(config.gms.appName)
-		.then(function(result)
-		{
-			logger.info('GET getdocid success::' + result, {module: 'routes'});
-			response.status(200).json(result);
+    .get(function(request, response) {
+        logger.info('GET getdocid for ' + config.gms.appName, { module: 'routes' });
+        worker.getDoc(config.gms.appName)
+            .then(function(result) {
+                logger.info('GET getdocid success::' + result, { module: 'routes' });
+                response.status(200).json(result);
 
-		})
-		.catch(function(error)
-		{
-			logger.error('GET getdocid route failure::' + error, {module: 'routes'});
-			response.status(400).json(error);
-		});
-	})
-	.post(function(request,response)
-	{
-		logger.info('POST getdocid for ' + request.body.appname, {module: 'routes'});
-		worker.getDoc(request.body.appname)
-		.then(function(result)
-		{
-			logger.info('POST getdocid success:: ' + result, {module: 'routes'});
-			response.status(200).json(result);
+            })
+            .catch(function(error) {
+                logger.error('GET getdocid route failure::' + error, { module: 'routes' });
+                response.status(400).json(error);
+            });
+    })
+    .post(function(request, response) {
+        logger.info('POST getdocid for ' + request.body.appname, { module: 'routes' });
+        worker.getDoc(request.body.appname)
+            .then(function(result) {
+                logger.info('POST getdocid success:: ' + result, { module: 'routes' });
+                response.status(200).json(result);
 
-		})
-		.catch(function(error)
-		{
-			logger.error('POST getdocid failure:: ' + error, {module: 'routes'});
-			response.status(400).json(error);
-		});
-	});
+            })
+            .catch(function(error) {
+                logger.error('POST getdocid failure:: ' + error, { module: 'routes' });
+                response.status(400).json(error);
+            });
+    });
 
 router.route('/add/all')
-	.post(function(request, response)
-	{
-		logger.info('POST add/all', {module: 'routes'});
-		worker.addAll()
-		.then(function(result)
-		{
-			logger.info('POST add/all success::' + result.result, {module: 'routes'});
-			response.status(200).json(result.result);
-		})
-		.catch(function(error)
-		{
-			logger.error('POST add/all failure::' + error, {module: 'routes'});
-			response.status(400).json(error);
-		});
+    .post(function(request, response) {
+        logger.info('POST add/all', { module: 'routes' });
+        worker.addAll()
+            .then(function(result) {
+                logger.info('POST add/all success::' + result.result, { module: 'routes' });
+                response.status(200).json(result.result);
+            })
+            .catch(function(error) {
+                logger.error('POST add/all failure::' + error, { module: 'routes' });
+                response.status(400).json(error);
+            });
 
-	});
+    });
 
 router.route('/update/all')
-	.post(function(request, response)
-	{
-		logger.info('POST update/all', {module: 'routes'});
-		worker.updateAll()
-		.then(function(result)
-		{
-			logger.info('POST update/all success::' + result.result, {module: 'routes'});
-				response.status(200).json(result.result + '\n');
-		})
-		.catch(function(error)
-		{
-			logger.error('POST update/all failure::' + error, {module: 'routes'});
+    .post(function(request, response) {
+        logger.info('POST update/all', { module: 'routes' });
+        worker.updateAll()
+            .then(function(result) {
+                logger.info('POST update/all success::' + result.result, { module: 'routes' });
+                response.status(200).json(result.result + '\n');
+            })
+            .catch(function(error) {
+                logger.error('POST update/all failure::' + error, { module: 'routes' });
 
-			var foo = {
-				result: 'POST update/all failure::' + error
-			};
+                var foo = {
+                    result: 'POST update/all failure::' + error
+                };
 
-			response.status(200).json(foo.result);
-		});
+                response.status(200).json(foo.result);
+            });
 
-	});
+    });
 
 router.route('/delete/fromapp')
-	.post(parseUrlencoded, function(request,response){
-		logger.info('POST delete/fromapp for ' + request.body.appname, {module: 'routes'});
-		worker.deleteFromApp(request.body)
-		.then(function(result)
-		{
-			logger.info('POST delete/all success::' + result.result, {module: 'routes'});
-			response.status(200).json(result.result + '\n');
-		});
-	});
+    .post(parseUrlencoded, function(request, response) {
+        logger.info('POST delete/fromapp for ' + request.body.appname, { module: 'routes' });
+        worker.deleteFromApp(request.body)
+            .then(function(result) {
+                logger.info('POST delete/all success::' + result.result, { module: 'routes' });
+                response.status(200).json(result.result + '\n');
+            });
+    });
 
 router.route('/reload')
-	.post(function(request, response)
-	{
-		logger.info('POST reload', {module: 'routes'});
-		worker.reloadMetricsApp()
-		.then(function(result)
-		{
-			logger.info('POST reload success::' + result.result, {module: 'routes'});
-			response.status(200).json(result.result);
-		})
-		.catch(function(error)
-		{
-			logger.error('POST reload failure::' + error, {module: 'routes'});
-			response.status(400).json(error);
-		});
-	});
+    .post(function(request, response) {
+        logger.info('POST reload', { module: 'routes' });
+        worker.reloadMetricsApp()
+            .then(function(result) {
+                logger.info('POST reload success::' + result.result, { module: 'routes' });
+                response.status(200).json(result.result);
+            })
+            .catch(function(error) {
+                logger.error('POST reload failure::' + error, { module: 'routes' });
+                response.status(400).json(error);
+            });
+    });
 
 router.route('/changeOwner')
-	.post(parseUrlencoded, function(request,response)
-	{
-		logger.info('POST changeOwner' ,{module: 'routes'});
-		worker.changeOwner(request.body)
-		.then(function(result)
-		{
-			response.status(200).json(result);
-		})
-		.catch(function(error)
-		{
-			response.status(400).json(error);
-		});
-	});
+    .post(parseUrlencoded, function(request, response) {
+        logger.info('POST changeOwner', { module: 'routes' });
+        worker.changeOwner(request.body)
+            .then(function(result) {
+                response.status(200).json(result);
+            })
+            .catch(function(error) {
+                response.status(400).json(error);
+            });
+    });
 
 router.route("/version")
-	.get(function(request, response)
-	{
-		response.status(200).send(config.gms.version);
-	});
+    .get(function(request, response) {
+        response.status(200).send(config.gms.version);
+    });
 
 //  Provide Master Data Items from specfic Qlik Sense applications, as an input
 //  to the metrics library. Hitting this endpoint should return all master data
 // 	items from the relevant Apps (either identified by name or tag in the
 //  mangement console).
 router.route("/getAllMDI")
-	.get(function(request, response)
-	{
-		var filter = !req.query.filter ? "mdi" : req.query.filter;
-		logger.info('GET getAllMdi with filer:' + filter, {module: 'routes'});
-		worker.getAllMdi(filter.toLowerCase()))
-		.then(function(result)
-		{
-			response.set({'Content-Type': 'application/json'})
-			logger.info('GET getAllMdi success::' + result, {module: 'routes'});
-			response.status(200).json(result + '\n');
-		})
-		.catch(function(error)
-		{
-			logger.error('POST getAllMdi failure::' + error, {module: 'routes'});
-			response.status(400).json(error);
-		})
-
-	});
+    .get(function(request, response) {
+        worker.getAllMdi()
+            .then(function(result) {
+                response.set({ 'Content-Type': 'application/json' })
+                logger.info('GET getAllMdi success::' + result, { module: 'routes' });
+                response.status(200).json(result + '\n');
+            })
+            .catch(function(error) {
+                logger.error('GET getAllMdi failure::' + error, { module: 'routes' });
+                response.status(400).json(error);
+            })
+    });
 
 
-function isEmpty(obj){
-	for(var prop in obj){
-		if(obj.hasOwnProperty(prop))
-		{
-			return false;
-		}
-	}
-	return true;
+function isEmpty(obj) {
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 module.exports = router;
