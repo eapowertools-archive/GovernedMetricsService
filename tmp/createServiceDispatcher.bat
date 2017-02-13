2ae84b26549ff13ff9053df7777e2bca005d03f4
Jeffrey Goldberg <JOG@qlik.com>
2017-02-13 16:30:28 -0500

    updates to service dispatcher batch files.

--- tmp/createServiceDispatcher.bat
+++ tmp/createServiceDispatcher.bat
@@ -3,9 +3,12 @@ sc query "QlikEAPowerToolsServiceDispatcher"
 IF %ERRORLEVEL% EQU 0 (GOTO END) ELSE (GOTO ADDSERVICE)
 
 :ADDSERVICE
-sc create QlikEAPowerToolsServiceDispatcher binPath= "%~1\PowerToolsServiceDispatcher\PowerToolsService.exe" DisplayName= "Qlik EAPowerTools Service Dispatcher" start= auto obj= %2 password= %3
+sc create QlikEAPowerToolsServiceDispatcher binPath= "%~1" DisplayName= "Qlik EAPowerTools Service Dispatcher" start= auto
 sc description "QlikEAPowerToolsServiceDispatcher" "Service Dispatcher for running EA Powertools" 
 
+sc start QlikEAPowerToolsServiceDispatcher
+
+TIMEOUT /T 10
 
 :END
 
--- utils/createServiceDispatcherManual.bat
+++ utils/createServiceDispatcherManual.bat
@@ -8,15 +8,12 @@ IF %ERRORLEVEL% EQU 0 (GOTO END) ELSE (GOTO ADDSERVICE)
 echo Enter the path and executable for the powertools service:
 set /P theBinPath=
 
-echo Enter the account the service will run as in domain\userid format:
-set /P theUserId= 
-
-echo Enter the password for the account that will run services: 
-set /P thePassword= 
-
-sc create QlikEAPowerToolsServiceDispatcher binPath= "%theBinPath%" DisplayName= "Qlik EAPowerTools Service Dispatcher" start= auto obj= %theUserId% password= %thePassword%
+sc create QlikEAPowerToolsServiceDispatcher binPath= "%theBinPath%" DisplayName= "Qlik EAPowerTools Service Dispatcher" start= auto
 sc description "QlikEAPowerToolsServiceDispatcher" "Service Dispatcher for running EA Powertools" 
 
+sc start QlikEAPowerToolsServiceDispatcher
+
+TIMEOUT /T 10
 
 :END
 
